53f4edbc26bf7e164f337db140a0102665746128
Leejjon <leejjon@gmail.com>
2021-03-07 23:43:10 +0100

    Nailed the server side title and description rendering.

--- node-runtime/index.ts
+++ node-runtime/index.ts
@@ -1,30 +1,83 @@
 'use strict';
 
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
+import ErrnoException = NodeJS.ErrnoException;
 
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+interface PageTitleAndDescription {
+    title: string;
+    description: string;
+}
+
+interface routeToPageAndDescriptionMapping {
+    [path: string]: PageTitleAndDescription;
+}
+
+const pageTitlesAndDescriptions: routeToPageAndDescriptionMapping = {
+    '/': {title: 'Home page', description: 'This is the home page.'},
+    '/news': {title: 'News page', description: 'This is the news page.'},
+    '/about': {title: 'About page', description: 'This is the about page.'}
+};
 
 // This code makes sure that any request that does not matches a static file
 // in the build folder, will just serve index.html. Client side routing is
 // going to make sure that the correct content will be loaded.
-app.use((req: Request, res: Response) => {
-    if (/(.ico|.js|.css|.jpg|.png)$/i.test(req.path)) {
-        res.status(404).send('Not found');
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        next();
+        console.log('Hoi');
     } else {
-        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-        res.header('Expires', '-1');
-        res.header('Pragma', 'no-cache');
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        const filePath = path.resolve(__dirname, 'build', 'index.html')
+        fs.readFile(filePath, 'utf8', (err: ErrnoException | null, data: string) => {
+            let title = pageTitlesAndDescriptions[req.path]?.title || 'Page not found';
+            let description = pageTitlesAndDescriptions[req.path]?.description || 'This is the page not found page.';
+
+            data = data.replace(/\$OG_TITLE/g, title);
+            data = data.replace(/\$OG_DESCRIPTION/g, description);
+
+            res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+            res.header('Expires', '-1');
+            res.header('Pragma', 'no-cache');
+            res.send(data);
+        });
     }
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 // Start the server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
 });
+
+/**
+ * Webpack HMR Activation
+ */
+
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+    hot?: {
+        data: any;
+        accept(
+            dependencies: string[],
+            callback?: (updatedDependencies: ModuleId[]) => void,
+        ): void;
+        accept(dependency: string, callback?: () => void): void;
+        accept(errHandler?: (err: Error) => void): void;
+        dispose(callback: (data: any) => void): void;
+    };
+}
+
+declare const module: WebpackHotModule;
+
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
+}
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Link} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 const about: React.FC = () => {
     return (
-        <div>About</div>
+        <div>
+            <Helmet>
+                <title>About page</title>
+                <meta name="description" content="This page explains everything about our react app."/>
+            </Helmet>
+            <div>About</div>
+        </div>
     );
 };
 
 const home: React.FC = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Helmet>
+                <title>Home page</title>
+                <meta name="description" content="This page is the home page."/>
+            </Helmet>
+            <div>Home</div>
+        </div>
     );
 };
 
 const news: React.FC = () => {
     return (
-        <div>News</div>
+        <div>
+            <Helmet>
+                <title>News page</title>
+                <meta name="description" content="This page shows the latest news."/>
+            </Helmet>
+            <div>News</div>
+        </div>
     );
 };
 
@@ -25,7 +44,7 @@ const App: React.FC = () => {
         <BrowserRouter>
             <div>
                 <Link to="/">Home</Link> <Link to="/about">About</Link> <Link to="/news">News</Link>
-                <br />
+                <br/>
                 <Route exact path="/" component={home}/>
                 <Route exact path="/about" component={about}/>
                 <Route exact path="/news" component={news}/>
